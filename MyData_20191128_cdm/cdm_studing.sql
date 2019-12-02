e8816d565c80d69dab9ec5dbc1fba4bf8d244a18
yonghoonlim <yonghoon.lim@sk.com>
2019-12-02 19:19:38 +0900

    at Mac

--- MyData_20191128_cdm/cdm_studing.sql
+++ MyData_20191128_cdm/cdm_studing.sql
@@ -50,6 +50,9 @@ create table care_cite as select distinct * from care_site_dup;
 select * from ohdsi_cdm.concept ;
 concept_id	concept_name	domain_id	vocabulary_id	concept_class_id	standard_concept	concept_code	valid_start_date	valid_end_date	invalid_reason
 
+-- concept.concept_id 가 실제 데이터에서 사용되는 코드값
+-- concept.concept_name 이 코드값명  --> 이것만 조인해도 값명은 모두 확인할 수 있는 구조
+
 select * from ohdsi_cdm.vocabulary; 
 select * from ohdsi_cdm.concept where concept_id = 44819232;
 select * from ohdsi_cdm.concept where vocabulary_id = 'Vocabulary';
@@ -57,5 +60,138 @@ select * from ohdsi_cdm.concept where vocabulary_id = 'Vocabulary';
 select     domain_id, vocabulary_id, concept_class_id, standard_concept, count(1) cnt from ohdsi_cdm.concept 
  group by  domain_id, vocabulary_id, concept_class_id, standard_concept  order by 1,2,3;
 
+select * from concept_ancestor ca order by 1,2;
 
+-- standaerd_concept 컬럼 : -- -> S는 데이터,  C는 Classify 용 컨
+-- This flag determines where 
+--   a Concept is a Standard Concept, i.e. is used in the data, 
+--                a Classification Concept, or a non-standard Source Concept. 
+--  The allowables values are 'S' (Standard Concept) and 'C' (Classification Concept), otherwise the content is NULL.
+
+-- ------------------------------------------------
+-- concept_ancestor
+-- 코드의 상하위 계층구조 구성
+-- 상하위가 없는 건은 상하 동일한 번호로 1건만 존재(하위가 있는 건도 동일 번호 하위가 존재....)
+select * from concept_ancestor ca order by 1,2;
+
+select ancestor_concept_id, c1.concept_name, descendant_concept_id, c2.concept_name
+  from concept_ancestor ca
+       inner join concept c1 on ca.ancestor_concept_id = c1.concept_id
+       inner join concept c2 on ca.descendant_concept_id = c2.concept_id
+ where ancestor_concept_id = 8515 -- Asian 
+ order by descendant_concept_id;
+
+select ancestor_concept_id, c1.concept_name, descendant_concept_id, c2.concept_name
+  from concept_ancestor ca
+       inner join concept c1 on ca.ancestor_concept_id = c1.concept_id
+       inner join concept c2 on ca.descendant_concept_id = c2.concept_id
+ where ancestor_concept_id = 38003585 -- Asian 
+ order by descendant_concept_id;
+
+
+-- ------------------------------------------------
+-- concept_synonym
+-- vocabulary 별로 (SNOMED, RxNorm ... ) 구분하는 역할인가... 
+
+select count(1) from concept_synonym cs ; -- 4898065
+select * from concept_synonym cs;
+
+select language_concept_id, count(1) cnt
+  from concept_synonym cs
+ group by language_concept_id
+ order by cnt desc;
+-- 4093769	2501091  
+-- 4180186	2396974
+select * from concept c where concept_id in (4093769,4180186); -- ?
+
+select * from concept_synonym cs where concept_synonym_name = 'gender'; -- 12건
+select cs.*, c1.*
+  from concept_synonym cs 
+       inner join concept c1 on cs.concept_id = c1.concept_id
+ where concept_synonym_name = 'gender';
+
+select ancestor_concept_id, c1.concept_name, descendant_concept_id, c2.concept_name
+  from concept_ancestor ca
+       inner join concept c1 on ca.ancestor_concept_id = c1.concept_id
+       inner join concept c2 on ca.descendant_concept_id = c2.concept_id
+ where ancestor_concept_id = 4135376 -- gender (s) 
+ order by descendant_concept_id; -- 아... 그런데도 하위에 male, female 이 없다... 
+ 
+
+select cs.*, c1.*
+  from concept_synonym cs 
+       inner join concept c1 on cs.concept_id = c1.concept_id
+ where concept_synonym_name = 'male';
+
+select ancestor_concept_id, c1.concept_name, descendant_concept_id, c2.concept_name
+  from concept_ancestor ca
+       inner join concept c1 on ca.ancestor_concept_id = c1.concept_id
+       inner join concept c2 on ca.descendant_concept_id = c2.concept_id
+ where descendant_concept_id = 8507 -- MALE (S)
+ order by descendant_concept_id; -- 상위에도 없다... 상위 개념이 
+ 
+select * from concept c where concept_class_id = 'ATC 1st'; -- 30건... 
+-- 21601237	CARDIOVASCULAR SYSTEM
+select ancestor_concept_id, c1.concept_name, descendant_concept_id, c2.concept_name
+  from concept_ancestor ca
+       inner join concept c1 on ca.ancestor_concept_id = c1.concept_id
+       inner join concept c2 on ca.descendant_concept_id = c2.concept_id
+ where ancestor_concept_id = 21601237 -- 흠... 1st 아래에는 자식 데이터가 많이 있군!!
+ order by descendant_concept_id;
+
+
+
+-- ------------------------------------
+-- relationship  관계 자체를 정의해 놓은 테이
+select count(1) from relationship; -- 388 건...
+select * from relationship; 
+
+-- ------------------------------------
+-- concept_relationship  
+-- 
+select count(1) from concept_relationship; -- 18450688 건...
+select * from concept_relationship; 
+
+select * from concept_relationship where concept_id_2 = 8507; -- MALE 
+select * from concept_relationship where concept_id_1 = 8507; -- MALE 
+select * from concept c where concept_id = 44814666; -- 44814666	Male	Observation	PCORNet 뭐 그다지...
+select c1.concept_name, c2.concept_name, cr.*
+  from concept_relationship cr
+       inner join concept c1 on cr.concept_id_1 = c1.concept_id
+       inner join concept c2 on cr.concept_id_2 = c2.concept_id
+ where concept_id_1 = 4135376; -- GENDER
+
+-- ------------------------------------
+-- source_to_concept_map
+select count(1) from source_to_concept_map; -- 0 건...
+
+-- ------------------------------------
+-- drug_strength : ingredient 별로 drug 의 양, 투여방법을 다 정의해 놓음... 통계에는 좋으나 유연하진 못할 것 같은데....
+select count(1) from drug_strength; -- 387222
+select * from drug_strength;
+
+select c1.concept_name, c2.concept_name, ds.*
+  from drug_strength ds
+       inner join concept c1 on ds.drug_concept_id = c1.concept_id
+       inner join concept c2 on ds.ingredient_concept_id = c2.concept_id
+;
+
+
+
+
+
+select * from person;
+
+-- ----------------------------
+-- visit_occurrence
+select * from visit_occurrence vo;
+
+select count(1), count(distinct person_id) from visit_occurrence vo; -- 5579542	99210
+
+select visit_concept_id, count(1) cnt from visit_occurrence vo group by visit_concept_id;
+select * from concept c where concept_id in (9201,9202); -- 입원, 외래 
+
+select visit_type_concept_id, count(1) cnt from visit_occurrence vo group by visit_type_concept_id;
+select * from concept c where concept_id in (44818517);
+select * from concept c where vocabulary_id = 'Visit Type'; -- 그리 안중요..
 
